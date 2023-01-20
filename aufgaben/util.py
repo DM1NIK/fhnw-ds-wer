60684918f761ba1187d17e4fb1accdd1664efe93
Noah <thenoahleuenberger@gmail.com>
2023-01-20 11:29:49 +0100

    Updated util

--- aufgaben/util.py
+++ aufgaben/util.py
@@ -38,7 +38,7 @@ def get_sem(std, n):
     return std / math.sqrt(n)
 
 
-def visualize_z_test(T, q, side, alpha):
+def visualize_z_test(T, q, side, significance):
     x = np.linspace(stats.norm.ppf(0.0001), stats.norm.ppf(0.9999), 100)
     plt.figure(figsize=(8, 5))
     plt.plot(x, stats.norm.pdf(x))
@@ -47,7 +47,7 @@ def visualize_z_test(T, q, side, alpha):
         plt.fill_between(x[x > q], 0, stats.norm.pdf(x)[x > q].flatten(), alpha=0.5, color='red')
         plt.fill_between(x[x < -q], 0, stats.norm.pdf(x)[x < -q].flatten(), alpha=0.5, color='red')
         plt.axvline(x=-T, color='r', linestyle='dashed')
-        plt.text(-q - 0.5, 0.05, 'Rejection region\n(' + str(alpha / 2) + ')', fontsize=10)
+        plt.text(-q - 0.5, 0.05, 'Rejection region\n(' + str(significance / 2) + ')', fontsize=10)
 
     elif side == "left":
         plt.fill_between(x[x < q], 0, stats.norm.pdf(x)[x < q].flatten(), alpha=0.5, color='red')
@@ -58,18 +58,18 @@ def visualize_z_test(T, q, side, alpha):
     plt.axvline(x=T, color='r', linestyle='dashed')
     plt.title(f"Z-Test {side} sided (T={T}, q={q})")
 
-    plt.text(q + 0.5, 0.05, 'Rejection region\n(' + str(alpha / 2) + ')', fontsize=10)
+    plt.text(q + 0.5, 0.05, 'Rejection region\n(' + str(significance / 2) + ')', fontsize=10)
     plt.show()
 
 
-def calculate_z_test(n, u, u0, std0, alpha, side="two", visualize=True):
+def calculate_z_test(n, u, u0, std0, significance, side="two", visualize=True):
     """
     Calculates the z-test for a given sample size, sample mean, population mean, population standard deviation and alpha level.
     :param n: sample size
     :param u: sample mean
     :param u0: population mean
     :param std0: population standard deviation
-    :param alpha: alpha level
+    :param significance: alpha level
     :param side: one of "two", "left", "right"
     :param visualize: whether to visualize the test
     :return: test statistic, critical value, p-value
@@ -77,21 +77,21 @@ def calculate_z_test(n, u, u0, std0, alpha, side="two", visualize=True):
 
     test_statistic = ((u - u0) / std0) * math.sqrt(n)
     if side == "two":
-        critical_val = stats.norm.ppf(1 - alpha / 2)
+        critical_val = stats.norm.ppf(1 - significance / 2)
         rejected = abs(test_statistic) > critical_val
 
     elif side == "left":
-        critical_val = -stats.norm.ppf(1 - alpha)
+        critical_val = -stats.norm.ppf(1 - significance)
         rejected = test_statistic < critical_val
 
     elif side == "right":
-        critical_val = stats.norm.ppf(1 - alpha)
+        critical_val = stats.norm.ppf(1 - significance)
         rejected = test_statistic > critical_val
     else:
         raise ValueError("side must be one of 'two', 'left', 'right'")
 
     if visualize:
-        visualize_z_test(test_statistic, critical_val, side, alpha)
+        visualize_z_test(test_statistic, critical_val, side, significance)
 
     print("H0: " + ("rejected" if rejected else "not rejected"))
 
@@ -99,7 +99,7 @@ def calculate_z_test(n, u, u0, std0, alpha, side="two", visualize=True):
     return test_statistic, critical_val, p
 
 
-def visualize_t_test(test_statistic, critical_val, side, alpha):
+def visualize_t_test(test_statistic, critical_val, side, confidence_level):
     x = np.linspace(stats.t.ppf(0.0001, 29), stats.t.ppf(0.9999, 29), 100)
     plt.figure(figsize=(8, 5))
     plt.plot(x, stats.t.pdf(x, 29))
@@ -109,7 +109,7 @@ def visualize_t_test(test_statistic, critical_val, side, alpha):
         plt.fill_between(x[x < -critical_val], 0, stats.t.pdf(x, 29)[x < -critical_val].flatten(), alpha=0.5,
                          color='red')
         plt.axvline(x=-test_statistic, color='r', linestyle='dashed')
-        plt.text(-critical_val - 0.5, 0.05, 'Rejection region\n(' + str(alpha / 2) + ')', fontsize=10)
+        plt.text(-critical_val - 0.5, 0.05, 'Rejection region\n(' + str(confidence_level / 2) + ')', fontsize=10)
 
     elif side == "left":
         plt.fill_between(x[x < critical_val], 0, stats.t.pdf(x, 29)[x < critical_val].flatten(), alpha=0.5, color='red')
@@ -120,18 +120,18 @@ def visualize_t_test(test_statistic, critical_val, side, alpha):
     plt.axvline(x=test_statistic, color='r', linestyle='dashed')
     plt.title(f"T-Test {side} sided (T={test_statistic}, q={critical_val})")
 
-    plt.text(critical_val + 0.5, 0.05, 'Rejection region\n(' + str(alpha / 2) + ')', fontsize=10)
+    plt.text(critical_val + 0.5, 0.05, 'Rejection region\n(' + str(confidence_level / 2) + ')', fontsize=10)
     plt.show()
 
 
-def calculate_t_test(n, u, u0, std0, alpha, side="two", visualize=True):
+def calculate_t_test(n, u, u0, std0, confidence_level, side="two", visualize=True):
     """
     Calculates the t-test for a given sample size, sample mean, population mean, population standard deviation and alpha level.
     :param n: sample size
     :param u: sample mean
     :param u0: population mean
     :param std0: population standard deviation
-    :param alpha: 1 - alpha level
+    :param confidence_level: alpha level
     :param side: one of "two", "left", "right"
     :param visualize: whether to visualize the test
     :return: test statistic, critical value, p-value
@@ -139,21 +139,21 @@ def calculate_t_test(n, u, u0, std0, alpha, side="two", visualize=True):
 
     test_statistic = ((u - u0) / std0) * math.sqrt(n)
     if side == "two":
-        critical_val = stats.t.ppf(1 - alpha / 2, n - 1)
+        critical_val = stats.t.ppf(1 - confidence_level / 2, n - 1)
         rejected = abs(test_statistic) > critical_val
 
     elif side == "left":
-        critical_val = -stats.t.ppf(1 - alpha, n - 1)
+        critical_val = -stats.t.ppf(1 - confidence_level, n - 1)
         rejected = test_statistic < critical_val
 
     elif side == "right":
-        critical_val = stats.t.ppf(1 - alpha, n - 1)
+        critical_val = stats.t.ppf(1 - confidence_level, n - 1)
         rejected = test_statistic > critical_val
     else:
         raise ValueError("side must be one of 'two', 'left', 'right'")
 
     if visualize:
-        visualize_t_test(test_statistic, critical_val, side, alpha)
+        visualize_t_test(test_statistic, critical_val, side, confidence_level)
 
     print("H0: " + ("rejected" if rejected else "not rejected"))
 
@@ -161,29 +161,29 @@ def calculate_t_test(n, u, u0, std0, alpha, side="two", visualize=True):
     return test_statistic, critical_val, p
 
 
-def calculate_binomial_test(n, p0, p, alpha, side="two"):
+def calculate_binomial_test(n, p0, p, confidence_level, side="two"):
     """
     Calculates the binomial test for a given sample size, sample probability, population probability and alpha level.
     Source: https://www.statisticshowto.com/probability-and-statistics/binomial-theorem/binomial-test/
     :param n: sample size
     :param p0: population probability
     :param p: sample probability
-    :param alpha: alpha level
+    :param confidence_level: alpha level
     :param side: one of "two", "left", "right"
     :return: test statistic, critical value, p-value
     """
 
     test_statistic = (p - p0) * math.sqrt(n / p0 * (1 - p0))
     if side == "two":
-        critical_val = stats.norm.ppf(1 - alpha / 2)
+        critical_val = stats.norm.ppf(1 - confidence_level / 2)
         rejected = abs(test_statistic) > critical_val
 
     elif side == "left":
-        critical_val = -stats.norm.ppf(1 - alpha)
+        critical_val = -stats.norm.ppf(1 - confidence_level)
         rejected = test_statistic < critical_val
 
     elif side == "right":
-        critical_val = stats.norm.ppf(1 - alpha)
+        critical_val = stats.norm.ppf(1 - confidence_level)
         rejected = test_statistic > critical_val
     else:
         raise ValueError("side must be one of 'two', 'left', 'right'")
@@ -271,3 +271,12 @@ def calculate_welch_t_test(x, y, confidence_level, side="two", visualize=True):
     print("H0: " + ("rejected" if rejected else "not rejected"))
 
     return test_statistic, critical_val, p
+
+def binomial_coefficient(n, k):
+    """
+    Calculates the binomial coefficient for a given n and k.
+    :param n: n
+    :param k: k
+    :return: binomial coefficient
+    """
+    return math.comb(n, k)
